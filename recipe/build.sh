fb771dced120a28dee54797ab04fb360ff7b60e1
Michael Sarahan <msarahan@gmail.com>
2017-10-05 21:05:38 -0500

    cb3 + replace DYLD_FALLBACK with RPATH setting

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-if [ `uname` == Darwin ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-fi
-
 cd trunk
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
--D CMAKE_SKIP_RPATH=ON \
--D HDF5_INCLUDE_DIR=$PREFIX/include \
--D HDF5_LIB_PATH=$PREFIX/lib \
-.
+      -D CMAKE_INSTALL_RPATH=$PREFIX/lib \
+      -D HDF5_INCLUDE_DIR=$PREFIX/include \
+      -D HDF5_LIB_PATH=$PREFIX/lib \
+      .
 
 make
 make install
